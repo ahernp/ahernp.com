73a5c9f3542ca0ffe765069488d60204e356c482
Paul Ahern <ahernp@ahernp.com>
2018-06-04 19:56:48 +0100

    Added old pages app

---
+++ pages/views.py
@@ -0,0 +1,70 @@
+import json
+
+from datetime import datetime
+
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+from pages.models import CONTENT_TYPE_CHOICES, Page
+
+
+def all_pages(request):
+    pages = Page.objects.all()
+    data = [page.dictionary for page in pages]
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def content_types(request):
+    data = [{"value": value, "label": label} for value, label in CONTENT_TYPE_CHOICES]
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+def read_page(request, slug):
+    try:
+        page = Page.objects.get(slug=slug)
+        data = page.dictionary
+    except Page.DoesNotExist:
+        data = {"title": "Page Not Found"}
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+@login_required
+def save_page(request):
+    new_page_dict = json.loads(request.body)
+
+    if "id" in new_page_dict and new_page_dict["id"]:
+        try:
+            page = Page.objects.get(pk=new_page_dict["id"])
+        except Page.DoesNotExist:
+            data = {"title": "Page Not Found"}
+    else:
+        page = Page.objects.create()
+
+    page.content = new_page_dict["content"]
+    page.title = new_page_dict["title"]
+    page.slug = new_page_dict["slug"]
+    page.parent_id = new_page_dict["parentId"]
+    page.content_type = new_page_dict["contentType"]
+    if "published" in new_page_dict and new_page_dict["published"]:
+        page.published = datetime.strptime(
+            new_page_dict["published"], "%Y-%m-%d"
+        ).date()
+    else:
+        page.published = None
+    page.updated = datetime.now()
+    page.save()
+
+    data = {}
+
+    return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+class HomePageView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context["DEBUG"] = settings.DEBUG
+        return context
--- project/settings.py
+++ project/settings.py
@@ -9,43 +9,44 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
@@ -60,22 +61,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -85,6 +80,4 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "static"))
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.normpath(os.path.join(BASE_DIR, "site_assets")),
-]
+STATICFILES_DIRS = [os.path.normpath(os.path.join(BASE_DIR, "site_assets"))]
