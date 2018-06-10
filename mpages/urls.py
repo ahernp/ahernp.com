91ec90786fc574e402ffd3999f98f3a6e5d1151e
Paul Ahern <ahernp@ahernp.com>
2018-06-10 08:53:53 +0100

    Make page lists filterable by parent

--- mpages/urls.py
+++ mpages/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.PageListView.as_view(), name="page-list"),
+    path("children/<slug:parent_slug>/", views.PageListView.as_view(), name="page-list"),
     path("<slug:slug>/", views.PageDetailView.as_view(), name="page-detail"),
 ]
--- mpages/views.py
+++ mpages/views.py
@@ -1,11 +1,19 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 
 from .models import Page
 
 
 class PageListView(ListView):
     model = Page
+
+    def get_queryset(self):
+        parent_slug = self.kwargs.get('parent_slug', None)
+        if parent_slug:
+            parent = get_object_or_404(Page, slug=parent_slug)
+            return Page.objects.filter(parent=parent)
+        return Page.objects.all()
 
 
 class PageDetailView(DetailView):
