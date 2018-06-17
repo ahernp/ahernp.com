01d84ca3ebd432adc56270fa140ea9c4efd06cce
Paul Ahern <ahernp@ahernp.com>
2018-06-17 11:24:19 +0100

    Allow linking directly to tool

--- tools/urls.py
+++ tools/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.ToolsView.as_view(), name="tools")]
+urlpatterns = [
+    path("", views.ToolsView.as_view(), name="tools"),
+    path("<slug:tool>/", views.ToolsView.as_view(), name="tool"),
+]
