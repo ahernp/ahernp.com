553e0c4a496c29fb86f5b5b6ae8e12abe900e057
Paul Ahern <ahernp@ahernp.com>
2018-06-26 21:01:14 +0100

    Extended text search to feedreader entries

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.db.models import F
 from django.views.generic.base import TemplateView
 
 from .utils import Headline
+from feedreader.models import Entry
 from mpages.models import Page
 
 
@@ -14,16 +15,26 @@ class SearchView(TemplateView):
         keywords = self.request.GET.get("search", "")
         if len(keywords) >= 3:
             pages = Page.objects.all()
+            entries = Entry.objects.all()
 
             search_query = SearchQuery(keywords)
             title_vector = SearchVector('title', weight='A')
             content_vector = SearchVector('content', weight='B')
-            vectors = title_vector + content_vector
-            pages = pages.annotate(search=vectors).filter(search=search_query)
-            pages = pages.annotate(rank=SearchRank(vectors, search_query)).order_by('-rank')
+            description_vector = SearchVector('description', weight='B')
+            page_vectors = title_vector + content_vector
+            entry_vectors = title_vector + description_vector
+
+            pages = pages.annotate(search=page_vectors).filter(search=search_query)
+            pages = pages.annotate(rank=SearchRank(page_vectors, search_query)).order_by('-rank')
             pages = pages.annotate(title_highlight=Headline(F('title'), search_query))
             pages = pages.annotate(content_highlight=Headline(F('content'), search_query))
             context["pages"] = pages
+
+            entries = entries.annotate(search=entry_vectors).filter(search=search_query)
+            entries = entries.annotate(rank=SearchRank(entry_vectors, search_query)).order_by('-rank')
+            entries = entries.annotate(title_highlight=Headline(F('title'), search_query))
+            entries = entries.annotate(description_highlight=Headline(F('description'), search_query))
+            context["entries"] = entries
         else:
             context["error"] = "Search term must be at least 3 characters"
         context["keywords"] = keywords
