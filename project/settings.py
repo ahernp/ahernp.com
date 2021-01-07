83b95678136d0b9817c4e325830bc3584afba979
Paul Ahern <ahernp@ahernp.com>
2021-01-07 17:34:42 +0000

    Updated Djanto to 3.1.5

--- project/settings.py
+++ project/settings.py
@@ -140,4 +140,4 @@ MAX_DAYS_SHOWN = 1
 MAX_DAYS_AGE = 3
 
 
-REST_FRAMEWORK = { 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema' }
+REST_FRAMEWORK = {"DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema"}
