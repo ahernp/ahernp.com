084930035675cb7e9ec2334da72affb532bc6582
Paul Ahern <ahernp@ahernp.com>
2021-04-11 10:20:49 +0100

    Simplify quiz url

--- quiz/urls.py
+++ quiz/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", view=views.QuizListView.as_view(), name="quiz-list"),
     path(
-        "questions/<int:pk>/",
+        "<int:pk>/",
         view=views.QuizDetailView.as_view(),
         name="quiz-detail",
     ),
