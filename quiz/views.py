8433292b33d5345401e4a273f95da275dcd8b844
Paul Ahern <ahernp@ahernp.com>
2021-04-15 20:25:23 +0100

    Simplify quiz detail view

--- quiz/views.py
+++ quiz/views.py
@@ -10,20 +10,3 @@ class QuizListView(ListView):
 
 class QuizDetailView(DetailView):
     model = Quiz
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        quiz = context["object"]
-        topics = []
-        questions_by_topic = {}
-        for question in quiz.question_set.all():
-            if question.topic not in topics:
-                topics.append(question.topic)
-            if question.topic.id not in questions_by_topic:
-                questions_by_topic[question.topic.id] = []
-            questions_by_topic[question.topic.id].append(question)
-        context["topics"] = [
-            {"topic": topic, "questions": questions_by_topic[topic.id]}
-            for topic in topics
-        ]
-        return context
