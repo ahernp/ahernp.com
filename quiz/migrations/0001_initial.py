ed8f1d03fc69c13238658710c94224d8799284ea
Paul Ahern <ahernp@ahernp.com>
2021-04-17 17:22:36 +0100

    Align radio buttons and labels

--- quiz/migrations/0001_initial.py
+++ quiz/migrations/0001_initial.py
@@ -8,47 +8,86 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Quiz',
+            name="Quiz",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.TextField()),
-                ('show_answers', models.BooleanField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.TextField()),
+                ("show_answers", models.BooleanField()),
             ],
             options={
-                'ordering': ['title'],
+                "ordering": ["title"],
             },
         ),
         migrations.CreateModel(
-            name='Topic',
+            name="Topic",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.TextField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.TextField()),
             ],
             options={
-                'ordering': ['title'],
+                "ordering": ["title"],
             },
         ),
         migrations.CreateModel(
-            name='Question',
+            name="Question",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('question', models.TextField()),
-                ('answer_a', models.TextField()),
-                ('answer_b', models.TextField()),
-                ('answer_c', models.TextField()),
-                ('answer_d', models.TextField()),
-                ('notes', models.TextField(blank=True)),
-                ('correct_answer', models.CharField(choices=[('a', 'A'), ('b', 'B'), ('c', 'C'), ('d', 'D')], max_length=1)),
-                ('quiz', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quiz.quiz')),
-                ('topic', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='quiz.topic')),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("question", models.TextField()),
+                ("answer_a", models.TextField()),
+                ("answer_b", models.TextField()),
+                ("answer_c", models.TextField()),
+                ("answer_d", models.TextField()),
+                ("notes", models.TextField(blank=True)),
+                (
+                    "correct_answer",
+                    models.CharField(
+                        choices=[("a", "A"), ("b", "B"), ("c", "C"), ("d", "D")],
+                        max_length=1,
+                    ),
+                ),
+                (
+                    "quiz",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="quiz.quiz"
+                    ),
+                ),
+                (
+                    "topic",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="quiz.topic"
+                    ),
+                ),
             ],
             options={
-                'ordering': ['quiz', 'question'],
+                "ordering": ["quiz", "question"],
             },
         ),
     ]
