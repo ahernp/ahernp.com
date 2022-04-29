03d5009ccc45ad49a954e135524c49ee6a587384
Paul Ahern <ahernp@ahernp.com>
2022-04-29 09:51:13 +0100

    Replace deprecated selenium find_element_by_* calls

--- integration_tests/test_admin.py
+++ integration_tests/test_admin.py
@@ -1,11 +1,13 @@
 import pytest
 
+from selenium.webdriver.common.by import By
+
 
 @pytest.mark.django_db
 def test_admin(admin_client_browser, live_server):
     url = f"{live_server.url}/admin/"
     admin_client_browser.get(url)
 
-    h1 = admin_client_browser.find_element_by_css_selector("h1")
+    h1 = admin_client_browser.find_element(By.TAG_NAME, "h1")
     assert h1.text == "Django administration"
     assert admin_client_browser.current_url == url
--- integration_tests/test_pages.py
+++ integration_tests/test_pages.py
@@ -2,6 +2,8 @@ import pytest
 
 import time
 
+from selenium.webdriver.common.by import By
+
 from django.urls import reverse
 
 from pages.factories import PageFactory
@@ -16,10 +18,10 @@ def test_page_list(browser, live_server):
     relative_url = reverse("page-list")
     browser.get(f"{live_server.url}{relative_url}")
 
-    h1 = browser.find_element_by_css_selector("h1")
+    h1 = browser.find_element(By.CSS_SELECTOR, "h1")
     assert h1.text == "Pages"
 
-    datatables_footer = browser.find_element_by_id("DataTables_Table_0_info")
+    datatables_footer = browser.find_element(By.ID, "DataTables_Table_0_info")
     assert datatables_footer.text == "Showing 1 to 1 of 1 entries"
 
 
@@ -29,7 +31,7 @@ def test_page_detail(browser, live_server):
     relative_url = reverse("page-detail", kwargs={"slug": page.slug})
     browser.get(f"{live_server.url}{relative_url}")
 
-    h1 = browser.find_element_by_css_selector("h1")
+    h1 = browser.find_element(By.CSS_SELECTOR, "h1")
     assert h1.text == PAGE_TITLE
 
 
@@ -38,24 +40,24 @@ def test_page_edit(admin_client_browser, live_server):
     page = PageFactory.create(content=f"# {PAGE_TITLE}")
     relative_url = reverse("page-detail", kwargs={"slug": page.slug})
     admin_client_browser.get(f"{live_server.url}{relative_url}")
-    admin_client_browser.find_element_by_link_text("Edit").click()
+    admin_client_browser.find_element(By.LINK_TEXT, "Edit").click()
 
-    h1 = admin_client_browser.find_element_by_css_selector("h1")
+    h1 = admin_client_browser.find_element(By.CSS_SELECTOR, "h1")
     assert h1.text == "Edit Markdown"
 
     # Edit page content
-    textarea = admin_client_browser.find_element_by_id("markdown")
+    textarea = admin_client_browser.find_element(By.ID, "markdown")
     textarea.clear()
     edited_content = f"## {PAGE_TITLE}"
     textarea.send_keys(edited_content)
 
     # Preview edited page content
-    admin_client_browser.find_element_by_id("preview-button").click()
-    h2 = admin_client_browser.find_element_by_css_selector("h2")
+    admin_client_browser.find_element(By.ID, "preview-button").click()
+    h2 = admin_client_browser.find_element(By.CSS_SELECTOR, "h2")
     assert h2.text == PAGE_TITLE
 
     # Save edited page content
-    admin_client_browser.find_element_by_xpath('//button[text()="Save"]').click()
+    admin_client_browser.find_element(By.XPATH, '//button[text()="Save"]').click()
     time.sleep(1)  # Wait for change to be saved to database
     page_from_database = Page.objects.get(pk=page.id)
     assert page_from_database.content == edited_content
