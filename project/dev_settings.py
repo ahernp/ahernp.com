e3ccb40b8ebbf54424bec19d721a38885eb65dfb
Paul Ahern <ahernp@ahernp.com>
2024-12-04 17:24:26 +0000

    Upgrade Django to 5.1.4

---
+++ project/dev_settings.py
@@ -0,0 +1,10 @@
+from .settings import *
+
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "fjgjgdgjsttigmvvmwpiegjsg")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "db.sqlite3",
+    }
+}
