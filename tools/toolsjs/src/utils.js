b2e0e33eeb281535861e5abcf946a995fb1b7cd1
Paul Ahern <ahernp@ahernp.com>
2025-09-19 19:35:52 +0100

    Remove node

--- tools/toolsjs/src/utils.js
+++
@@ -1,6 +0,0 @@
-export function copyToClipboard(id) {
-    let copyText = document.getElementById(id);
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-};
